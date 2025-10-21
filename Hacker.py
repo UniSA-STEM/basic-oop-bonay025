a3637968eed18cb6a90ebff9d08f7c27445b98dd
bonay025 <bonay025@mymail.unisa.edu.au>
2025-10-21 16:12:08 +1100

    Created Hacker class and defined to init and string conversion methods.

--- Hacker.py
+++ Hacker.py
@@ -6,3 +6,17 @@ ID: 110457542
 Username: bonay025
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Hacker:
+    """This class represents a Hacker object."""
+    def __init__(self, name, inventory):
+        self.__name = name
+        self.__inventory = []
+        self.__trace_level = 0
+        self.__exposed = False
+
+    def __str__(self):
+        return (f"Name: {self.__name} \n"
+                f"Rig: \n"
+                f"Trace Level: {self.__trace_level} \n"
+                f"Inventory: {self.__inventory}")
