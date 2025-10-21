7ad2dcfa044f7aebf31afe56438f15b490216444
bonay025 <bonay025@mymail.unisa.edu.au>
2025-10-21 15:51:29 +1100

    Created Asset class and defined the init and string conversion method.

--- Asset.py
+++ Asset.py
@@ -6,3 +6,15 @@ ID: 110457542
 Username: bonay025
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Asset:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.encrypted = False
+
+    def __str__(self):
+        if not self.encrypted:
+            return f"{self.name}: {self.description} [Encrypted]"
+        else:
+            return f"{self.name}: {self.description}"
