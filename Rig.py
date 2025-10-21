d74a41e12d6254efcff312655150e6917c4bedf0
bonay025 <bonay025@mymail.unisa.edu.au>
2025-10-21 16:17:02 +1100

    Created Rig class and defined the init and string conversion methods.

--- Rig.py
+++ Rig.py
@@ -6,3 +6,18 @@ ID: 110457542
 Username: bonay025
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Rig:
+    """This class represents a Rig(computer) object."""
+    def __init__(self, name):
+        self.__name = name
+        self.__damage_counter = 0
+        self.__broken_state = False
+        self.__storage = []
+        self.__upgrade_level = 0
+
+    def __str__(self):
+        return (f"{self.__name} \n"
+                f"{self.__broken_state} \n"
+                f"{self.__upgrade_level} \n"
+                f"{self.__storage} \n")
