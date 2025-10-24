86bf3ebe68d65ae12e098bd9653a5f0353a97b29
bonay025 <bonay025@mymail.unisa.edu.au>
2025-10-24 18:33:56 +1100

    Tests for Asset string conversion method and storing and retrieving assets between a hacker and its rig.

--- Main.py
+++ Main.py
@@ -6,3 +6,38 @@ ID: 110457542
 Username: bonay025
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+from Hacker import Hacker
+from Asset import Asset
+from Rig import Rig
+
+
+# Tests that the string conversion method for both encrypted and decrypted for assets
+def asset_test():
+    asset = Asset("CryptoToken", "Used to acquire or repair rigs.")
+    print(asset)
+    asset.set_encrypted(True)
+    print(asset)
+
+
+def test_rig_repair():
+    nix = Hacker("Nix")
+    nix.acquire_rig("Nova")
+    nix.repair_rig()
+    titanium = Rig("Titanium")
+    nix.launch_data_spike("Titanium")
+    print(titanium)
+
+
+# Test the store and retrieve methods
+# Shows the cases of it an asset is not in storage and inventory
+def test_store_and_retrieve():
+    nix = Hacker("Nix")
+    nix.acquire_rig("Nova")
+    print(nix)
+    nix.retrieve_asset(Asset("CryptoToken", ""))
+    nix.retrieve_asset(Asset("Data Spike", "Used in battles."))
+    print(nix)
+    nix.store_asset(Asset("CryptoToken", "Used to acquire or repair rigs."))
+    nix.store_asset(Asset("Data Spike", "Used in battles."))
+    print(nix)
